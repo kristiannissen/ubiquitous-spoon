443cf59ffbcb70517aba93e85e8af18e07f4a8f1
Kristian <kn@unisport.dk>
2019-06-11 13:59:44 +0200

    Socket setup and effects

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 /**
  * @file server.js
  */
-var connect = require("connect"),
+var app = require("connect")(),
   serveStatic = require("serve-static"),
-  path = require("path");
+  path = require("path"),
+  http = require("http").createServer(app),
+  io = require("socket.io")(http);
 
-connect()
-  .use(serveStatic(path.join(__dirname, "dist")))
-  .listen(3000, function() {
-    console.log("Server is running");
-  });
+app.use(serveStatic(path.join(__dirname, "dist")));
+
+io.on("connection", (socket) => {
+    console.log("client connected")
+
+    socket.on("chat message", (data) => {
+        console.log("chat "+ data.msg)
+        io.emit("chat message", data.msg)
+    })
+
+    socket.on("disconnect", () => console.log("client disconnected"))
+})
+
+http.listen(3000, () => console.log("Server is running"));
--- src/components/calendar.js
+++ src/components/calendar.js
@@ -1,18 +1,28 @@
 /**
  * @file src/components/calendar.js
  */
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import io from "socket.io-client"
 
 import {CalendarContext} from '../context'
 
-class Calendar extends React.Component {
-    render () {
-        let props = this.props
-        let calendar = this.context
-        return <div>Hello Kitty</div>
-    }
+const Calendar = (props) => {
+    const [msg, setMsg] = useState("")
+
+    useEffect(() => {
+        console.log("effect "+ msg)
+
+        const socket = io()
+        socket.emit("chat message", {msg: "Kitty"})
+        socket.on("chat message", (data) => setMsg(data))
+        console.log("effect "+ msg)
+    })
+
+    return (
+        <div>Hello {msg}</div>
+    )
 }
 
-Calendar.contextType = CalendarContext
+// Calendar.contextType = CalendarContext
 
 export default Calendar;
